503ced6c6301f65eadf0cfd009cba87a1df84cc8
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-03 14:18:43 +0900

    status 받아오는 방식 변경

--- src/main/java/com/sparta/Jmt/controller/UserController.java
+++ src/main/java/com/sparta/Jmt/controller/UserController.java
@@ -4,6 +4,7 @@ import com.sparta.Jmt.dto.MsgResponseDto;
 import com.sparta.Jmt.dto.UserRequestDto;
 import com.sparta.Jmt.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,12 @@ public class UserController {
     @PostMapping("/jmt/sign-up")
     public MsgResponseDto signup(@RequestBody UserRequestDto requestDto) {
         userService.signup(requestDto);
-        return new MsgResponseDto("회원가입 성공", 200);
+        return new MsgResponseDto("회원가입 성공", HttpStatus.CREATED.value());
     }
 
     @PostMapping("/jmt/login")
     public MsgResponseDto login(@RequestBody UserRequestDto requestDto, HttpServletResponse res) {
         userService.login(requestDto, res);
-        return new MsgResponseDto("로그인 성공", 200);
+        return new MsgResponseDto("로그인 성공", HttpStatus.CREATED.value());
     }
 }
