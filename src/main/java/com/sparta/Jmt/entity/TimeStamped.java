c4d61bc154de31532d581106f9603f4a061a5cbb
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-03 19:07:45 +0900

    줄 맞춤

--- src/main/java/com/sparta/Jmt/entity/TimeStamped.java
+++ src/main/java/com/sparta/Jmt/entity/TimeStamped.java
@@ -8,18 +8,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-    @Getter
-    @MappedSuperclass
-    @EntityListeners(AuditingEntityListener.class)
-    public abstract class TimeStamped{
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeStamped {
 
-        @CreatedDate
-        @Column(updatable = false)
-        @Temporal(TemporalType.TIMESTAMP)
-        private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
 
-        @LastModifiedDate
-        @Column
-        @Temporal(TemporalType.TIMESTAMP)
-        private LocalDateTime modifiedAt;
-    }
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
