0e0f5f2e39e79f3daf541a3ae47d53837f48e63a
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-03 16:44:35 +0900

    response 수정

--- src/main/java/com/sparta/Jmt/dto/MsgResponseDto.java
+++ src/main/java/com/sparta/Jmt/dto/MsgResponseDto.java
@@ -1,8 +1,10 @@
 package com.sparta.Jmt.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MsgResponseDto {
     private String message;
     private Integer statusCode;
