5fca8c770a7d63a9b8d7e1fd11335878a5c4b3ef
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-03 12:05:49 +0900

    User entity 추가

--- src/main/java/com/sparta/Jmt/entity/User.java
+++ src/main/java/com/sparta/Jmt/entity/User.java
@@ -1,4 +1,35 @@
 package com.sparta.Jmt.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column
+    private String profileComment;
+
+    @Column
+    private URL profileImage;
 }
