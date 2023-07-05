4b550ae4938fc881b0b0e5e3c2f22a33c2ba3697
Ahn Jeongmin <symbol9550@gmail.com>
2023-07-05 09:52:14 +0900

    Annotation 수정

--- src/main/java/com/sparta/Jmt/controller/PostController.java
+++ src/main/java/com/sparta/Jmt/controller/PostController.java
@@ -4,9 +4,11 @@ import com.sparta.Jmt.dto.MsgResponseDto;
 import com.sparta.Jmt.dto.PostListResponseDto;
 import com.sparta.Jmt.dto.PostRequestDto;
 import com.sparta.Jmt.dto.PostResponseDto;
+import com.sparta.Jmt.security.UserDetailsImpl;
 import com.sparta.Jmt.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.RejectedExecutionException;
@@ -18,45 +20,46 @@ public class PostController {
     //의존성 주입 받기위해 Service를 빈으로 등록 후 받아온다.
     //우리가 사용한 방법 생성자 주입
     private final PostService postService;
-    public PostController(PostService postService){
+
+    public PostController(PostService postService) {
         this.postService = postService;
     }
 
     //게시물 작성
     @PostMapping("/post")
-    public ResponseEntity<PostResponseDto> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody PostRequestDto requestDto){
+    public ResponseEntity<PostResponseDto> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody PostRequestDto requestDto) {
         PostResponseDto result = postService.createPost(requestDto, userDetails.getUser());
         return ResponseEntity.status(201).body(result);
     }
 
     @GetMapping("/posts")//글 전체 조회
-    public ResponseEntity<PostListResponseDto> getPosts(){
+    public ResponseEntity<PostListResponseDto> getPosts() {
         PostListResponseDto result = postService.getPosts();
         return ResponseEntity.ok().body(result);
     }
 
     @GetMapping("/post/{postId}")//글 단건 조회
-    public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long postId){
+    public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long postId) {
         PostResponseDto result = postService.getPostById(postId);
         return ResponseEntity.ok().body(result);
     }
 
     @PutMapping("/post/{postId}")
-    public ResponseEntity<PostResponseDto> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @RequestBody PostRequestDto requestDto){
-        try{
+    public ResponseEntity<PostResponseDto> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @RequestBody PostRequestDto requestDto) {
+        try {
             PostResponseDto result = postService.updatePost(postId, requestDto, userDetails.getUser());
             return ResponseEntity.ok().body(result);
-        }catch(RejectedExecutionException e) {
+        } catch (RejectedExecutionException e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @DeleteMapping("/post/{postId}")
-    public ResponseEntity<MsgResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId){
-        try{
+    public ResponseEntity<MsgResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId) {
+        try {
             postService.deletePost(postId, userDetails.getUser());
             return ResponseEntity.ok().body(new MsgResponseDto("게시글 삭제 성공", HttpStatus.OK.value()));
-        }catch(RejectedExecutionException e) {
+        } catch (RejectedExecutionException e) {
             return ResponseEntity.badRequest().build();
         }
     }
