ea1da85bc46523420c0c099e5a276c3c90229c2d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-06 21:38:00 +0900

    Post 정보를 반환할때 해당 Post Comment List도 같이 반환.
    CommentResponseDto를 List 형식으로 반환하기 위해 for문 사용.

--- src/main/java/com/sparta/Jmt/dto/PostResponseDto.java
+++ src/main/java/com/sparta/Jmt/dto/PostResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.Jmt.dto;
 
+import com.sparta.Jmt.entity.Comment;
 import com.sparta.Jmt.entity.Post;
 import com.sparta.Jmt.entity.PostCategory;
 import jakarta.validation.constraints.NotBlank;
@@ -9,11 +10,13 @@ import lombok.Setter;
 
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class PostResponseDto {
-    private String userName;
+    private String userNick;
 
     @NotBlank(message = "글의 제목을 입력해주세요.")
     private String postTitle;
@@ -32,9 +35,10 @@ public class PostResponseDto {
 
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+    private List<CommentResponseDto> comments;
 
     public PostResponseDto(Post post) {
-        this.userName = post.getUser().getUserName();
+        this.userNick = post.getUser().getUserNick();
         this.postTitle = post.getPostTitle();
         this.postContent = post.getPostContent();
         this.postCategory = post.getPostCategory();
@@ -45,5 +49,11 @@ public class PostResponseDto {
         this.jmtMenuImageUrl = post.getJmtMenuImageUrl();
         this.createdAt = post.getCreatedAt();
         this.modifiedAt = post.getModifiedAt();
+        List<CommentResponseDto> commentList = new ArrayList<>();
+        for (Comment comment : post.getComments()) {
+            CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+            commentList.add(commentResponseDto);
+        }
+        this.comments = commentList;
     }
 }
