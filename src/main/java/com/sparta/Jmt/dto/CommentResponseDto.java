63fa339629405fbf58641c67f68f33ff95c60da9
alxqkekrroqkfwk <fexxet@icloud.com>
2023-07-07 11:27:38 +0900

    CommentResponseDto에 POSTID값 추가

--- src/main/java/com/sparta/Jmt/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/Jmt/dto/CommentResponseDto.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 public class CommentResponseDto {
     private final String userName;
     private final String Content;
+    private final Long postId;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
     public CommentResponseDto(Comment comment) {
         this.userName = comment.getUser().getUserName();
         this.Content = comment.getContent();
+        this.postId = comment.getPost().getPostId();
         this.createdAt = comment.getCreatedAt();
         this.modifiedAt = comment.getModifiedAt();
     }
